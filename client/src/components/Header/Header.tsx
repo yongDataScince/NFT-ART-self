9acbaa2186b4af268fc90b8b7c09a7c208df894e
yongDataScince <61432518+yongDataScince@users.noreply.github.com>
2022-08-13 20:28:34 +0300

    fixes

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -5,19 +5,20 @@ import useOnClickOutside from '../../hooks/useClickOutside'
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../store';
 import CopyIcon from '../UI/icons/CopyIcon';
+import SettingsIcon from '../UI/icons/SettingsIcon';
 
 export const Header: React.FC<{ os: string }> = ({ os }) => {
-  const [open, setOpen] = useState<boolean>(false);
   const ref = useRef<null | HTMLDivElement>(null)
   const { haveEth } = useAppSelector((state) => state.web3)
   const navigate = useNavigate()
-
-  useOnClickOutside(ref, () => setOpen(false))
 
   return (
     <Styled.HeaderBar>
       <Styled.HeaderButton>
         <HomeIcon />
+      </Styled.HeaderButton>
+      <Styled.HeaderButton>
+        <SettingsIcon />
       </Styled.HeaderButton>
       {(os === 'iOS' || os === 'Android') && (
         <Styled.HeaderButton style={{ marginLeft: 20 }} onClick={() => window.open('https://metamask.app.link/dapp/da376751.nft-art-preview.pages.dev/')}>
